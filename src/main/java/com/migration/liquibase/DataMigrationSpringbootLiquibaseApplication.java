798236b3c6e91f9b580719c346e8554f96738427
Tejraj Nimbalkar <nimbalkarts@gmail.com>
2026-03-03 18:50:16 +0530

    Initial Setup

---
+++ src/main/java/com/migration/liquibase/DataMigrationSpringbootLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.migration.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMigrationSpringbootLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMigrationSpringbootLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migration/liquibase/DataMigrationSpringbootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migration.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigrationSpringbootLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
